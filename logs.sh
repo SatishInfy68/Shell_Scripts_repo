4f0c47bfa96b64fa9fac8321ec8257e45351522c
SatishInfy68 <satishlcs527@gmail.com>
2025-02-17 20:08:28 +0530

    sehll script

--- logs.sh
+++ logs.sh
@@ -5,6 +5,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d:%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/LOG_FILE-$TIMESTAMP.log"
+
+echo "Script is started at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -23,21 +29,21 @@ then
         exit 1
 fi
 
-dnf list installed git*
+dnf list installed git* &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-    dnf install git* -y
+    dnf install git* -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing GIT"
 else
     echo -e "GIT is already...$Y INSTALLED"
 fi
 
-dnf list installed mariadb*
+dnf list installed mariadb* &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then 
-    dnf install mariadb* -y
+    dnf install mariadb* -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing MARIADB"
 else
     echo -e "MARIADB is already...$Y INSTALLED"
